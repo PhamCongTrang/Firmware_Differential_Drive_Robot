a5179bb3bc35ce8c1597529f5dba6b89d8167839
unknown <trang.pc200631@sis.hust.edu.vn>
2023-04-03 13:49:18 +0700

    Delete LED - BUTTON sub - pub

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include <ros.h>
-#include <std_msgs/String.h>
-#include <std_msgs/UInt16.h>
 #include <Arduino.h>
 
 #define ENA 8
@@ -10,38 +8,8 @@
 #define IN3 34
 #define IN4 36
 
-#define BUTTON 8
-#define LED 13
-
-ros::NodeHandle node_handle;
-
-std_msgs::String button_msg;
-std_msgs::UInt16 led_msg;
-
-void subscriberCallback(const std_msgs::UInt16 &led_msg)
-{
-    if (led_msg.data == 1)
-    {
-        digitalWrite(LED, HIGH);
-    }
-    else
-    {
-        digitalWrite(LED, LOW);
-    }
-}
-
-ros::Publisher button_publisher("button_press", &button_msg);
-ros::Subscriber<std_msgs::UInt16> led_subscriber("toggle_led", &subscriberCallback);
-
 void setup()
 {
-    pinMode(LED, OUTPUT);
-    pinMode(BUTTON, INPUT);
-
-    node_handle.initNode();
-    node_handle.advertise(button_publisher);
-    node_handle.subscribe(led_subscriber);
-
     pinMode(ENA, OUTPUT);
     pinMode(ENB, OUTPUT);
     pinMode(IN1, OUTPUT);
@@ -52,18 +20,6 @@ void setup()
 
 void loop()
 {
-    if (digitalRead(BUTTON) == HIGH)
-    {
-        button_msg.data = "Pressed";
-    }
-    else
-    {
-        button_msg.data = "NOT pressed";
-    }
-
-    button_publisher.publish(&button_msg);
-    node_handle.spinOnce();
-
     analogWrite(ENA, 160);
     analogWrite(ENB, 160);
     digitalWrite(IN1, HIGH);
