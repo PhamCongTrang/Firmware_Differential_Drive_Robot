259398f66331b4c630342791201a854c4a42d909
unknown <trang.pc200631@sis.hust.edu.vn>
2023-04-03 14:30:01 +0700

    Build function struct and add variable necessary

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,20 @@
 #define IN2 32
 #define IN3 34
 #define IN4 36
+#define A1 18
+#define B1 19
+#define A2 20
+#define B2 21
+
+float v; //angular.x (m/s)
+int pret = 0; // Temp of time 
+int cycle = 100; // cycle to read encoder & calculate PID (ms)
+float vr_set, vl_set; // Speed left & right setting (m/s)
+float vr_mea, vl_mea; // Speed left & right measuring (m/s)
+int duty_left, duty_right; // Duty of PWM pulse. Range from -100 to 100 (%)
+float Kp, Ki, Kd; // PID parameter
+float P, I, D; // Value of Proportional Integral Differential
+
 
 void setup()
 {
@@ -17,7 +31,22 @@ void setup()
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
 }
+int calculate_vright(float v, float omega)
+{
 
+}
+int calculate_vleft(float v, float omega)
+{
+
+}
+int PID(float vr_set, float vr_mea)
+{
+
+}
+int hash_PWM(int duty_left, int duty_right)
+{
+
+}
 void loop()
 {
     analogWrite(ENA, 160);
@@ -26,5 +55,4 @@ void loop()
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN2, LOW);
-    delay(100);
 }
