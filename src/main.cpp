4f9b4347fed3672fbcaf58050e8e68f5ba7bb81a
unknown <trang.pc200631@sis.hust.edu.vn>
2023-04-04 22:39:13 +0700

    coarse tuning PID

--- src/main.cpp
+++ src/main.cpp
@@ -22,11 +22,11 @@ float omega; //angular.z(rad/s) subcribe
 float vBack; //linear.x (m/s) publish
 float omegaBack;//angular.z(rad/s) publish
 unsigned long pret = 0; // Temp of time 
-int cycle = 100; // cycle to read encoder & calculate PID (ms)
+int cycle = 400; // cycle to read encoder & calculate PID (ms)
 float vr_set, vl_set; // Speed left & right setting (m/s)
 float vr_mea, vl_mea; // Speed left & right measuring (m/s)
 int duty_left, duty_right; // Duty of PWM pulse. Range from -255 to 255;
-float Kp = 100, Ki = 0, Kd = 0; // PID parameter
+float Kp = 12, Ki = 1, Kd = 2; // PID parameter
 float P, I = 0, D; // Value of Proportional Integral Differential
 float L = 0.235; // distance between 2 wheel (m)
 float r_wheel = 0.05; // radian of wheel (m)
@@ -77,7 +77,7 @@ void setup()
     attachInterrupt(CA1I, encoder_counter_left, RISING); 
     attachInterrupt(CA2I, encoder_counter_right, RISING); 
 
-    //Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 float measure_speed(long int cnt, long int pre_cnt)
@@ -109,7 +109,7 @@ int PID(float v_set, float v_mea)
 }
 void hash_PWM(int duty_left, int duty_right)
 {
-    duty_left = (int) duty_left*1.2;
+    //duty_left = (int) duty_left*1.2;
     while ((millis() - pret) < cycle)
     {
         if(duty_left >= 0)
@@ -137,38 +137,39 @@ void hash_PWM(int duty_left, int duty_right)
             digitalWrite(IN4, HIGH);
         }
     }
+    //delay(cycle);
     pret = millis();
 }
 void loop()
 {
-    nh.spinOnce();
+    //nh.spinOnce();
     // delay(1);
-    //v = 0.5;
-    //omega = 0.5;
+    v = 1;
+    omega = 0;
     //Serial.print("cnt_l, cnt_r"); Serial.print(cnt_l); Serial.print("  "); Serial.println(cnt_r);
 
     vr_set = calculate_vright(v, omega);
     vl_set = calculate_vleft(v, omega);
-    //Serial.print("vl_set, vr_set"); Serial.print(vl_set); Serial.print("  "); Serial.println(vr_set);
+    Serial.print("vl_set, vr_set"); Serial.print(vl_set); Serial.print("  "); Serial.println(vr_set);
 
     vr_mea = measure_speed(cnt_r, pre_cnt_r);
     pre_cnt_r = cnt_r;
     vl_mea = measure_speed(cnt_l, pre_cnt_l);
     pre_cnt_l = cnt_l;
-    //Serial.print("vl_mea, vr_mea"); Serial.print(vl_mea); Serial.print("  "); Serial.println(vr_mea); 
+    Serial.print("vl_mea, vr_mea"); Serial.print(vl_mea); Serial.print("  "); Serial.println(vr_mea); 
     
-    // duty_left += PID(vl_set, vl_mea);
-    // duty_right += PID(vr_set, vr_mea);
-    duty_left =(int) 100*(vl_set - vl_mea);
-    duty_right =(int) 100*(vr_set - vr_mea);
+    duty_left += PID(vl_set, vl_mea);
+    duty_right += PID(vr_set, vr_mea);
+    // duty_left +=(int) 20*(vl_set - vl_mea);
+    // duty_right +=(int) 20*(vr_set - vr_mea);
 
     if (duty_left > 100) duty_left = 100;
         if (duty_left < -100) duty_left = -100;
     if (duty_right > 100) duty_right = 100;
         if (duty_right < -100) duty_right = -100;
 
-    //Serial.print("duty_left, duty_right"); Serial.print(duty_left); Serial.print("  "); Serial.println( duty_right);
-
+    Serial.print("duty_left, duty_right"); Serial.print(duty_left); Serial.print("  "); Serial.println( duty_right);
+    Serial.print("Time calculate"); Serial.println(millis() - pret);
     hash_PWM(duty_left, duty_right);
 
     vBack = (vr_mea + vl_mea)/2;
@@ -177,6 +178,6 @@ void loop()
     velBack.linear.x = vBack;
     velBack.angular.z = omegaBack;
     pubvel.publish(&velBack);
-    //Serial.println("-------------------");
-    delay(1);
+    Serial.println("-------------------");
+    //delay(1);
 }
