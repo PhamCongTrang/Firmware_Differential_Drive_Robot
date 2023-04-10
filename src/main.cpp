c32a5f78dc105566ead9a495adf460f8c5a5f6f9
leddat <datht1112@gmail.com>
2023-04-10 08:04:40 +0700

    10/4

--- src/main.cpp
+++ src/main.cpp
@@ -138,6 +138,7 @@ void hash_PWM(int duty_left, int duty_right)
 void loop()
 {
     nh.spinOnce();
+    delay(1);
 
     vr_set = calculate_vright(v, omega);
     vl_set = calculate_vleft(v, omega);
@@ -158,5 +159,5 @@ void loop()
     velBack.linear.x = vBack;
     velBack.angular.z = omegaBack;
     pubvel.publish(&velBack);
-    delay(1);
+    
 }
